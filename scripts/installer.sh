3e9f7e3794726d755cfdce4c97e9b97a843769f9
Aiwe <maksymivv@gmail.com>
2020-07-23 14:40:54 +0300

    Fix installer script

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -1,15 +1,1 @@
-#!/usr/bin/env bash
-
-if [ ! -d src ]
-then
-mkdir src
-fi
-
-cd Karbo-webwallet-js
-npm install
-node ./node_modules/typescript/bin/tsc --project tsconfig.prod.json
-node build.js
-cd ../
-
-cp -r Karbo-webwallet-js/src/. src
-cp -r config/main.js src
+#!/usr/bin/env bashif [ ! -d src ]thenmkdir srcficd Karbo-webwallet-jsnpm installnode ./node_modules/typescript/bin/tsc --project tsconfig.prod.jsonnode build.jscd ../cp -r Karbo-webwallet-js/src/. srccp -r config/main.js src
